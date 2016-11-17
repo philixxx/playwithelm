3200aad19d89749b0d4f23b61765d9bb6314668c
Philippe Martinez <philippe.martinez@sysfera.com>
2016-11-17 17:15:29 +0100

    minor : css and text

--- src/Reservation/View.elm
+++ src/Reservation/View.elm
@@ -33,8 +33,8 @@ viewMap model =
     header
         [ class "header" ]
         [ div []
-            [ button [ onClick <| LoadData ] [ text "Load Data for Reservation" ]
+            [ button [ onClick <| LoadData, class "btn ink-reaction btn-raised btn-primary" ] [ text "Load Data for Reservation" ]
             , Html.App.map BasketMsg (Basket.View.view model.basket)
-            , button [ Html.Attributes.id "prereserve-button", onClick <| Pay ] [ text "Pay" ]
+            , button [ Html.Attributes.id "prereserve-button", class "btn ink-reaction btn-raised btn-primary", onClick <| Pay ] [ text "Valider" ]
             ]
         ]
