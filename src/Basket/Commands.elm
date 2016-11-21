b21d654cb953ebf142ebd73cd306a0331685cc28
dway8 <diane.maniere@gmail.com>
2016-11-21 23:13:11 +0100

    fetchQuote: http post json to string

--- src/Basket/Commands.elm
+++ src/Basket/Commands.elm
@@ -3,19 +3,16 @@ module Basket.Commands exposing (..)
 import Http
 import Task
 import Json.Decode as Decode exposing ((:=))
+import Json.Encode
 import Basket.Messages exposing (Msg(..))
 import Basket.Models exposing (..)
+import Payment.Commands exposing (reservationEncoder)
 
 
 fetchQuote : Basket -> Cmd Msg
-fetchQuote quoteRequest =
-    Http.post quoteDecoder quoteRequest.endpoint (quoteRequestToBody quoteRequest)
+fetchQuote basket =
+    Http.post quoteDecoder basket.endpoint (Http.string (reservationEncoder basket |> Json.Encode.encode 0))
         |> Task.perform FetchQuoteFail FetchQuoteDone
-
-
-quoteRequestToBody : Basket -> Http.Body
-quoteRequestToBody quoteRequest =
-    Http.string """{"profile": "PARTICULIER", "exhibitorStatus": "RESIDENT", "spots": ["graphid123", "graphid456", "graphid789"]}"""
 
 
 quoteDecoder : Decode.Decoder Quotation
